bfe71a77c7b9cf93d53d794c9ddf2e12638b4149
vendetta60 <115636143+vendetta60@users.noreply.github.com>
2026-02-09 09:16:16 +0400

    Delete part2 directory

--- part2/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import warnings
-# This setsup pytest file
-
-def pytest_configure():
-    warnings.filterwarnings(
-        "ignore",
-        category=DeprecationWarning,
-        message=r".*jsonschema\.RefResolver.*"
-    )
